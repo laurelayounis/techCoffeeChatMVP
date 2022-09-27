6d901853c011d9f8006acd627bd66ef913f21e1a
Laurel Younis <laurelyounisdev@gmail.com>
2022-09-26 19:18:26 -0700

    initial commit

---
+++ client/src/pages/OnBoarding.js
@@ -0,0 +1,235 @@
+import { useState } from "react"
+import Nav from "../components/Nav"
+import { useCookies } from "react-cookie"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+const OnBoarding= () =>{
+    const [cookies, setCookie, removeCookie] = useCookies(null)
+    const [formData, setFormData] = useState({
+        user_id: cookies.UserId,
+        first_name:'',
+        last_name:'',
+        dob_day:'',
+        dob_month:'',
+        dob_year:'',
+        show_gender: false,
+        offer: 'coffee chat',
+        interest:'conference buddy',
+        email: cookies.Email,
+        url:'',
+        about:'',
+        matches: []
+    })
+
+    let navigate= useNavigate()
+
+    const handleSubmit= async (e) => {
+        console.log('submitted')
+        //prevent from reloading because its a form
+        e.preventDefault()
+        try {
+            const response= await axios.put('http://localhost:8000/user', {formData})
+            const success= response.status === 200
+            if (success) navigate('/dashboard')
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+//pass through e for event  make sure name and value are correct on form inputs otherwise wont work (need to get the name and input of each
+//value so we can save its name and value in our db)
+//handle checked box if true get e.tagert.checked : e.target.value
+    const handleChange= (e) => {
+        const value= e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        const name= e.target.name
+
+        //update new value to our db; get the previous state and update it to its current value using name
+        //can now use formData with their values on our forms below 
+        setFormData((prevState) => ({
+            ...prevState,
+            [name] : value
+        })) 
+    }
+        console.log(formData)
+    return(
+
+        <>
+      
+          <Nav 
+          minimal={true} 
+          setShowModal={() => {}} 
+          showModal={false} 
+          />
+            <div className="onboarding">
+                <h2>CREATE ACCOUNT</h2>
+
+                <form onSubmit={handleSubmit}>
+
+                    <section>
+
+                    <label htmlFor="first_name">First Name</label>
+                    <input
+                        id="first_name"
+                        type="text"
+                        name="first_name"
+                        placeholder="First Name"
+                        required={true}
+                        value={formData.first_name}
+                        onChange={handleChange}
+                    />
+
+                    
+                    <label htmlFor="last_name">Last Name</label>
+                    <input
+                        id="last_name"
+                        type="text"
+                        name="last_name"
+                        placeholder="Last Name"
+                        required={true}
+                        value={formData.last_name}
+                        onChange={handleChange}
+                    />
+
+
+                    <label>Birthday</label>
+
+                    <div className="multiple-input-container">
+
+                    <input
+                        id="dob_day"
+                        type="number"
+                        name="dob_day"
+                        placeholder="DD"
+                        required={true}
+                        value={formData.dob_day}
+                        onChange={handleChange}
+                    />
+                    <input
+                        id="dob_month"
+                        type="number"
+                        name="dob_month"
+                        placeholder="MM"
+                        required={true}
+                        value={formData.dob_month}
+                        onChange={handleChange}
+                    />
+                    <input
+                        id="dob_year"
+                        type="number"
+                        name="dob_year"
+                        placeholder="YYYY"
+                        required={true}
+                        value={formData.dob_year}
+                        onChange={handleChange}
+                    />
+                    </div>
+
+                    <label>Link me with:</label>
+                    <div className="multiple-input-container">
+                    <input
+                        id="interest-coffee-chat"
+                        type="radio"
+                        name="interest"
+                        value="coffee chat"
+                        onChange={handleChange}
+                        checked={formData.interest === 'coffee chat'}
+                    /> 
+                        <label htmlFor="interest-coffee-chat">Coffee Chat</label>
+
+                    <input
+                        id="interest-conference-buddy"
+                        type="radio"
+                        name="interest"
+                        value="conference buddy"
+                        onChange={handleChange}
+                        checked={formData.interest === 'conference buddy'}
+                    />
+                         <label htmlFor="woman-gender-identity">Conference Buddy</label>
+
+
+                    <input
+                        id="interest-both"
+                        type="radio"
+                        name="interest"
+                        value="both"
+                        onChange={handleChange}
+                        checked={formData.interest === 'both'}
+                    />
+                         <label htmlFor="interest-both">Both</label>
+                    </div>
+                    
+          
+                    <label>I'm offering to link up for:</label>
+                    <div className="multiple-input-container">
+                    <input
+                        id="offer-coffee"                                            //man-gender-identity        offer 
+                        type="radio"
+                        name="offer"                                            //gender_identity           offer
+                        value="coffee chat"                                     //man                       coffee chat
+                        onChange={handleChange}
+                        checked={formData.offer === 'coffee chat'}
+                    /> 
+                        <label htmlFor="offer-coffee">Coffee Chat</label>
+  
+                    <input
+                        id="offer-buddy"
+                        type="radio"
+                        name="interest"
+                        value="conference buddy"
+                        onChange={handleChange}
+                        checked={formData.offer === 'conference buddy'}
+                    />
+                         <label htmlFor="offer-buddy">Conference Buddy</label>
+
+
+                    <input
+                        id="offer-both"
+                        type="radio"
+                        name="interest"
+                        value="both"
+                        onChange={handleChange}
+                        checked={formData.offer === 'both'}
+                    />
+                         <label htmlFor="offer-both">Both</label>
+                    </div>
+
+                    <label htmlFor="about">About Me</label>
+                    <input
+                        id="about"
+                        type="text"
+                        name="about"
+                        required={true}
+                        placeholder="I'm a software engineer, I love to go hiking and find new trails."
+                        value={formData.about}
+                        onChange={handleChange}
+                    />
+                    <input type="submit"/>
+                </section>
+
+                <section>  
+                    <label htmlFor="url">Profile Photo</label>
+                         <input
+                            type="url"
+                            name="url"
+                            id="url"
+                            onChange={handleChange}
+                            required={true}
+                        />
+                    <div className="photo-container">
+                        {formData.url && <img src={formData.url} alt="profile picture preview" />}
+
+                    </div>
+                </section>
+                   
+
+                </form>
+
+            </div>
+
+     
+        </>
+    )
+}
+
+export default OnBoarding
---
+++ server/index.js
@@ -0,0 +1,278 @@
+const PORT= 8000
+const express= require('express')
+const {MongoClient}= require('mongodb')
+const {v4: uuidv4}= require('uuid')
+const jwt= require('jsonwebtoken')
+const bcrypt= require('bcrypt')
+const cors= require('cors')
+require('dotenv').config()
+
+const uri= process.env.URI
+
+
+const app= express()
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+//ROUTES
+
+//DEFAULT
+app.get('/', (req, res) => {
+    res.json('hello to my app')
+})
+
+//sign up to the databse
+//POST:   send data POST request; how we're passing through data from the front end to the back end signup form
+app.post('/signup', async (req, res) => {
+    const client= new MongoClient(uri)
+    const {email, password}= req.body
+    //generate a unique user id using the uuid package
+    const generatedUserId= uuidv4()
+    const hashedPassword= await bcrypt.hash(password, 10)
+
+
+
+    //send the data to the db
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+
+        //check to make sure user doesn't already exist when they signup
+        const existingUser= await users.findOne({email})
+
+        if(existingUser){
+            return res.status(409).send('User already exists, please login')
+        }
+        //if user doesn't exist start by sanitizing the email
+        const sanitizedEmail= email.toLowerCase()
+
+        //create data obj this will be the data we want to send to our db
+        const data= {
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+        }
+
+
+
+        //insert our new users into the db
+       const insertedUser= await users.insertOne(data)
+
+       //create token for user with jwt package
+       const token= jwt.sign(insertedUser, sanitizedEmail, {
+        expiresIn: 60 *24,
+       })
+       //this is what we'll send back
+       res.status(201).json({ token, userId: generatedUserId})
+    }catch(err){
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
+
+
+
+//Login to the database POST 
+app.post('/login', async (req, res) => {
+    const client= new MongoClient(uri)
+    const {email, password} = req.body
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+
+        const user= await users.findOne({email})
+
+        const correctPassword= await bcrypt.compare(password, user.hashed_password)
+
+        if(user && correctPassword){
+            const token= jwt.sign(user, email, {
+                expiresIn: 60* 24
+            })
+            res.status(201).json({ token, userId: user.user_id})
+        }
+        res.status(400).send('Invalid Credentials')
+    } catch(err){
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
+
+
+
+//Get individual user  GET
+
+app.get('/user', async (req, res) => {
+    const client= new MongoClient(uri)
+    const userId= req.query.userId
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+
+        const query= {user_id: userId}
+        const user= await users.findOne(query)
+        res.send(user)
+    } finally{
+        await client.close()
+    }
+})
+
+// Update User with a match
+app.put('/addmatch', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {userId, matchedUserId} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: userId}
+        const updateDocument = {
+            $push: {matches: {user_id: matchedUserId}}
+        }
+        const user = await users.updateOne(query, updateDocument)
+        res.send(user)
+    } finally {
+        await client.close()
+    }
+})
+
+//Get all the users by userIds in the Database GET
+
+app.get('/users', async (req, res) => {
+    const client= new MongoClient(uri)
+    const userIds= JSON.parse(req.query.userIds)
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+
+        const pipeline=
+        [
+            {
+                '$match':{
+                    'user_id':{
+                        '$in': userIds
+                    }
+                }
+            }
+        ]
+        const foundUsers= await users.aggregate(pipeline).toArray()
+        console.log(foundUsers)
+        res.send(foundUsers)
+    }finally{
+        await client.close()
+    }
+})
+
+
+
+
+//Get all the interest Users in the database   GET  //need to update *********************
+app.get('/interest-users', async(req, res) => {
+    const client= new MongoClient(uri)
+    /*********need to change to interests   plural??? **///
+    const interests= req.query.interests  ///need to make .interests plural?
+    console.log('interests', interests)
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+        const query= {offer: {$eq:interests}}                     ///////////////////////
+        const foundUsers= await users.find(query).toArray()
+        /***need to change .send to .json??? */
+        res.send(foundUsers)
+    }finally{
+        await client.close()
+    }
+})
+
+//update a User in the Database     PUT
+app.put('/user', async (req, res) => {
+    const client= new MongoClient(uri)
+    const formData= req.body.formData
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const users= database.collection('users')
+        //query the db
+        const query= {user_id: formData.user_id}
+
+        const updateDocument= {
+            $set:{
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                offer: formData.offer,  //update this ////////////
+                interest: formData.interest,                //update this //////////////
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+
+            },
+        }
+        const insertedUser= await users.updateOne(query, updateDocument)
+
+        res.json(insertedUser)
+
+    } finally {
+        await client.close()
+    }
+})
+
+//Get messages by: from_userId and to_userId
+app.get('/messages', async (req, res) => {
+    const {userId, correspondingUserId}= req.query
+    const client= new MongoClient(uri)
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const messages= database.collection('messages')
+
+        const query= {
+            from_userId: userId, to_userId: correspondingUserId
+        }
+        const foundMessages= await messages.find(query).toArray()
+        res.send(foundMessages)
+    }finally{
+        await client.close()
+    }
+})
+
+
+//Add a message to our Database POST
+app.post('/message', async (req, res) => {
+    const client= new MongoClient(uri)
+    const message= req.body.message
+
+    try{
+        await client.connect()
+        const database= client.db('app-data')
+        const messages= database.collection('messages')
+
+        const insertedMessage= await messages.insertOne(message)
+        res.send(insertedMessage)
+    }finally{
+        await client.close()
+    }
+})
+
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
